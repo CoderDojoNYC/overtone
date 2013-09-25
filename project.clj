22d6c1953a8ca4b2cdeefcff3ce33328b1a85416
Ehtesh Choudhury <merlockmagus@gmail.com>
2013-09-24 23:19:50 -0400

    Added :main     - resolves 'No :main namespace specified in project.clj'     - makes `lein run` work.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main coderdojonyc.core
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [overtone "0.8.1"]])
