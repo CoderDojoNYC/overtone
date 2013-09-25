1158af7c1ffac1f1b6b2b48ef0ef1de041b8c428
Ben Sussman <benjamin.sussman@gmail.com>
2013-09-24 20:28:06 -0400

    pull lib stuff out into a lib

--- bennosplayground/src/bennosplayground/core.clj
+++ bennosplayground/src/bennosplayground/core.clj
@@ -1,27 +1,7 @@
 (ns bennosplayground.core
     (:gen-class))
 (use 'overtone.live)
-
-;; We use a saw-wave that we defined in the oscillators tutorial
-(definst saw-wave [freq 440 attack 0.01 sustain 0.1 release 0.01 vol 0.8] 
-  (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
-     (saw freq)
-     vol))
-
-;; Let's make easier to pass notes
-(defn saw-by-note [music-note]
-    (saw-wave (midi->hz (note music-note))))
-
-(defn play-chord [a-chord]
-  (doseq [note a-chord] (saw-by-note note)))
-
-; this function will play our sound at whatever tempo we've set our metronome to 
-(defn looper [nome sound current-beat increment final-beat]
-    (let [beat-time (nome current-beat)]
-        (at beat-time (sound))
-	(when (> final-beat current-beat)
-	 (looper nome sound (+ current-beat increment) increment final-beat)
-        )))
+(use 'bennosplayground.lib)
 
 (defn mario-theme [m beat-num]
     (println 'mario-theme)
@@ -61,6 +41,7 @@
   (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
      (white-noise) ; also have (white-noise) and others...
      vol))
+
 (definst noisey-p [freq 420 attack 0.004 sustain 0.1 release 0.04 vol 0.4] 
   (* (env-gen (lin-env attack sustain release) 1 1 0 1 FREE)
      (pink-noise) ; also have (white-noise) and others...
