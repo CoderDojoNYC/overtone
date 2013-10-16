61ddb433430e81f8e430aaccc5af0b5184ba8fbf
unknown <Ehtesh Choudhury>
2013-10-16 16:11:12 -0400

    Added and removed following libraries:
    + org.clojure/data/json
    + lein-light-nrepl
    - org.clojure/clojure
    
    Update .m2 directory to reflect new libraries, so users won't have to
    download.

--- project.clj
+++ project.clj
@@ -4,5 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main coderdojonyc.core
+
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone "0.8.1"]])
+                 [lein-light-nrepl "0.0.1"]
+                 [overtone "0.8.1" :exclusions [org.clojure/clojure]]
+                 [org.clojure/data.json "0.2.3"]]
+
+  :repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops]})
